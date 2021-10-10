43853ade40c0b86b216945ca4abd246ff9cb9265
sooo19 <smile_cindy@naver.com>
2021-10-10 18:15:00 +0900

    File I/O 메모장문제(error)

--- java_study/src/java_study/TextFile_error.java
+++ java_study/src/java_study/TextFile_error.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+// soorim
 // 숫자를 메모장 파일에 입력하고, 다시 불러오면 이상한 문자 기호(기계어로 추정)로 불러와지는데, 그 이유와 해결 방안을 아직 모르겠다.
 // 오류 원인과 해결방안은 error_study에 올릴 예정
 
