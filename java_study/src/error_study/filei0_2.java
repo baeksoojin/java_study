bad65166a484fae15db718a0bfcf6ffa76fc81d2
sujin-create <qsoojinq@naver.com>
2021-10-12 00:44:07 +0900

    FileOutputStream과 Scanner

---
+++ java_study/src/error_study/filei0_2.java
@@ -0,0 +1,46 @@
+package error_study;
+
+import java.io.*;
+import java.util.*;
+
+public class filei0_2 {
+public static void main(String[] args) throws IOException{
+		
+		Scanner first = new Scanner(new FileInputStream("C:\\Users\\백수진\\OneDrive\\바탕 화면\\3학년2학기\\모바일소프트웨어\\first.txt"));
+		Scanner second = new Scanner(new FileInputStream("C:\\Users\\백수진\\OneDrive\\바탕 화면\\3학년2학기\\모바일소프트웨어\\second.txt"));
+		//FileInputStream을 Scanner로 받아와서 자유롭게 형식을 바꿔서 사용함. => 더 쉽게 동작가능.
+		int first_array[] = new int[10];
+		int second_array[] = new int[10];
+		
+		int i=0;
+		while(first.hasNext()) {
+			first_array[i] = first.nextInt();
+			i++;
+		}
+		i=0;
+		while(second.hasNext()) {
+			second_array[i] = second.nextInt();
+			i++;
+		}
+		
+		FileOutputStream os = new FileOutputStream("C:\\Users\\백수진\\OneDrive\\바탕 화면\\3학년2학기\\모바일소프트웨어//result.txt");
+		
+		String data = "";
+		for(int j=0; j<first_array.length; j++) {
+			for(int k=0; k<second_array.length; k++) {
+				if(first_array[j]==second_array[k]) {
+					System.out.print(first_array[j]+" ");
+					data +=first_array[j]+" ";
+					//os.write(f_array[j]+" ");을 사용한다면 이상한 문자가 파일에 저장됨.
+					//FileOutputStream은 문자열로 입출력을 하며 byte단위로 바꿔줘야함. getBytes()를 활용.
+				}
+			}
+		}
+		
+		os.write(data.getBytes());
+		os.close();
+		
+		
+}
+
+}
