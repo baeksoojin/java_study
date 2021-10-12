9364f8a5d389dcb35bac43b49626dfe09b9bd826
sooo19 <smile_cindy@naver.com>
2021-10-12 14:32:25 +0900

    1316

--- java_study/src/baekjoon/group_number_1316_YSR.java
+++ java_study/src/baekjoon/group_number_1316_YSR.java
@@ -13,29 +13,29 @@ public class group_number_1316_YSR {
 		
 		for(int i=0; i<N; i++) {
 			word=scan.next();
-			System.out.println("입력받은 단어:"+word);
+			//System.out.println("입력받은 단어:"+word);
 			
-			String[] alphabet;
+			String[] alphabet=new String[100];
 			alphabet=word.split("");	//입력받은 단어를 각 자리별로 쪼개서 배열에 저장
-			
+			/*
 			for(int a=0; a<alphabet.length; a++) {
 				System.out.print(alphabet[a]);
 			}
+            */
 			
-			System.out.println();
 			for (int j=0; j<alphabet.length-1; j++) {	//j=alphabet 배열의 length가 되면, 그 다음 자리의 알파벳과 비교해야하는데 해당 값이 없으므로 오류가 뜬다.
 				if(alphabet[j]!=alphabet[j+1]) {
 					int Num=num;	//Num: 원래의 num 숫자
 					for(int k=j+2; k<alphabet.length; k++) {
 						if(alphabet[j].equals(alphabet[k])) {	//그룹 단어X
-							System.out.println(alphabet[j]+"=="+alphabet[k]);
+							//System.out.println(alphabet[j]+"=="+alphabet[k]);
 							num-=1;
-							System.out.println("num="+num);
+							//System.out.println("num="+num);
 							break;
 						}
 						else
 						{
-							System.out.println(alphabet[j]+"!="+alphabet[k]);
+							//System.out.println(alphabet[j]+"!="+alphabet[k]);
 						}
 					}
 					if(num==Num-1) {	//그룹단어 count 했으면, for문 빠져 나가기(중복 count 막기 위함)
