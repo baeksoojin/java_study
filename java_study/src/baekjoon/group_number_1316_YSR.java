6344c2d316a0f63a397e1c5181c9cc9fb28a5d80
sooo19 <smile_cindy@naver.com>
2021-10-15 17:03:09 +0900

    group number

--- java_study/src/baekjoon/group_number_1316_YSR.java
+++ java_study/src/baekjoon/group_number_1316_YSR.java
@@ -1,45 +1,56 @@
 java.util.Scanner; //스캐너 사용
 import java.util.Scanner;	//스캐너 사용을 위한 import문
 
-//아직 하는중(틀림)
-
-public class group_number_1316_YSR {
+//아직 하는 중(틀림)
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		int N=scan.nextInt();	//스캐너 사용 방법 외우기
-		String word;	//입력받을 단어
-		int num=N;	//그룹 단어의 개수 세기
+		int N=scan.nextInt();
+		String word;
+		int num=N;
 		
 		for(int i=0; i<N; i++) {
 			word=scan.next();
-			//System.out.println("입력받은 단어:"+word);
-			
 			String[] alphabet=new String[100];
-			alphabet=word.split("");	//입력받은 단어를 각 자리별로 쪼개서 배열에 저장
-			/*
-			for(int a=0; a<alphabet.length; a++) {
-				System.out.print(alphabet[a]);
-			}
-            */
+			alphabet=word.split("");
+
 			
-			for (int j=0; j<alphabet.length-1; j++) {	//j=alphabet 배열의 length가 되면, 그 다음 자리의 알파벳과 비교해야하는데 해당 값이 없으므로 오류가 뜬다.
-				if(alphabet[j]!=alphabet[j+1]) {
-					int Num=num;	//Num: 원래의 num 숫자
-					for(int k=j+2; k<alphabet.length; k++) {
-						if(alphabet[j].equals(alphabet[k])) {	//그룹 단어X
-							//System.out.println(alphabet[j]+"=="+alphabet[k]);
-							num-=1;
-							//System.out.println("num="+num);
+			for (int j=0; j<alphabet.length-1; j++) {
+				//System.out.println("--2");
+				if(alphabet[j].equals(alphabet[j+1])) {
+					//System.out.println("--1");
+					int Num=num;
+					int a=j+1;
+					while(alphabet[j].equals(alphabet[a]))
+					{
+							//연속되는 중복 값 바로 다음 처음으로 나오는 다른 값의 배열 번호 = a
+							System.out.println(alphabet[j]+"="+alphabet[a]);
+							a++;
+							if(a==alphabet.length) {
+								break;
+							}
+					}
+					for(int k=a+1; k<alphabet.length; k++) {
+						if(alphabet[j].equals(alphabet[k])) { //haapa, haaapa, haaap
+							System.out.println(alphabet[j]+"="+alphabet[k]);
+							num--;
 							break;
 						}
-						else
-						{
-							//System.out.println(alphabet[j]+"!="+alphabet[k]);
-						}
 					}
-					if(num==Num-1) {	//그룹단어 count 했으면, for문 빠져 나가기(중복 count 막기 위함)
+					if(num==Num-1) {
 						break;
+					}
+				}
+				else {
+					for(int l=0; l<alphabet.length-2; l++) // 2.hapa, hapaa
+					{
+						int Num=num;
+						if(alphabet[l].equals(alphabet[l+2])) {
+							System.out.println(alphabet[l]+"="+alphabet[l+2]);
+							num--;
+							break;
+						}
 					}
 				}
 			}
@@ -47,5 +58,6 @@ public class group_number_1316_YSR {
 		System.out.print(num);
 		scan.close();
 	}
-
 }
+
+//중복되는 횟수 잘 빼기 -> break 해서
