4c195ec7c43d3744c61e8e24178804aa0964682a
sujin-create <qsoojinq@naver.com>
2021-10-17 02:44:43 +0900

    game을 위한 캐릭터(상속과 오버라이딩)

---
+++ java_study/src/java_study/Barlog.java
@@ -0,0 +1,60 @@
+package java_study;
+
+public class Barlog extends Character {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		this.hitPoint=170;
+		this.portionNumber=5;
+		
+	}
+
+	@Override
+	public void attck(Character c) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		if(Math.random()<=0.2) {
+			sum+=35;
+		}
+		else sum+=25;
+		if(Math.random()<=0.2) {
+			System.out.print(c.getDamage(sum));
+		}
+		else System.out.print(c.getDamage(sum));
+	}//2번 가능.
+
+	@Override
+	public boolean needPortion() {
+		// TODO Auto-generated method stub
+		if((this.hitPoint<=100 && this.hitPoint>0) && this.portionNumber>0)
+		{
+			return true;
+		}
+		else return false;
+	}//Barlog역시 상대방의 공격에 의한 데미지로 죽수 있을 때가 70의 경우가 시작. 70을 기준으로함.
+
+	@Override
+	public void recover() {
+		// TODO Auto-generated method stub
+			this.portionNumber--;
+			this.hitPoint+=30;
+	
+	}
+
+	@Override
+	public void whoAmI() {
+		// TODO Auto-generated method stub
+		System.out.print("<Barlog>");
+		
+	}
+
+	@Override
+	public int getDamage(int damage) {
+		// TODO Auto-generated method stub
+
+		this.hitPoint -= damage;
+		return damage;
+	}
+
+}
---
+++ java_study/src/java_study/CyberDaemon.java
@@ -0,0 +1,59 @@
+package java_study;
+
+public class CyberDaemon extends Character {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		this.hitPoint=250;
+		this.portionNumber=5;
+		
+	}
+
+	@Override
+	public void attck(Character c) {
+		// TODO Auto-generated method stub
+		if(Math.random()<=0.3) {
+			System.out.print(c.getDamage(60));
+		}
+		else {
+			System.out.print(c.getDamage(40));
+		}
+	}
+
+	@Override
+	public boolean needPortion() {
+		// TODO Auto-generated method stub
+		if((this.hitPoint<=180 && this.hitPoint>0) && this.portionNumber>0)
+		{
+			return true;
+		}
+		else return false;
+	}//CyberDaemon을 제외한 나머지 캐릭터중 공격중 최대치로 입을 수 있는 타격이 70이기에. 70이하가 되면 물약을 먹음.\
+
+	@Override
+	public void recover() {
+		// TODO Auto-generated method stub
+		this.portionNumber--;
+		if(Math.random()<=0.3) {
+			this.hitPoint+=50;
+		}
+		else {
+			this.hitPoint+=30;
+		}
+	}
+
+	@Override
+	public void whoAmI() {
+		// TODO Auto-generated method stub
+		System.out.print("<CyberDaemon>");
+	}
+
+	@Override
+	public int getDamage(int damage) {
+		// TODO Auto-generated method stub
+		this.hitPoint -= damage;
+		return damage;
+	}
+
+}
